0c9e4cc1d4365ef8fb0adaa323c69f7aaec0a4be
Catalin Torge <torgeadelin@gmail.com>
2019-11-19 01:02:03 +0000

    fix icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
         background_color: `#FFFFFF`,
         theme_color: `#000000`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/memoji.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
